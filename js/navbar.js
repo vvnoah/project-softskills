125b3d03fe71b013cd2e51d0bee44f1819296884
KilianLouerr <r0881575@ucll.be>
2023-03-05 12:00:08 +0100

    Zondere Jquery

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,9 @@
-$(function(){
-    $("#nav-placeholder").load("../html/navbar.html");
-});
+/**
+ * Met internet kan je jquery gebruiken.
+ */
+// $(function(){
+//     $("#nav-placeholder").load("../html/navbar.html");
+// });
 
 function toggle() {
     const foldmenu = document.getElementById("foldmenu");
@@ -10,3 +13,27 @@ function toggle() {
       foldmenu.style.display = "block";
     }
   }
+
+  /**
+   * Zonder internet gebruik je vanilla javascript.
+   */
+const navPlaceholder = document.querySelector("#nav-placeholder");
+navPlaceholder.innerHTML = `
+  <link rel="stylesheet" href="/css/navbar.css">
+  <nav class="mainNav">
+  <ul id="listNav">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="personalPage.html">Personal page</a></li>
+    <li><a href="examples.html">Examples</a></li>
+    <li id="hamburger" onclick="toggle()"><a><img src="/data/Hamburger_iconWit.png"></a></li>
+  </ul>
+
+  <div id="foldmenu">
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="personalPage.html">Personal page</a></li>
+      <li><a href="examples.html">Examples</a></li>
+    </ul>
+  </div>
+  </nav>
+`;
